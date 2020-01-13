9a0cd4c7c09b9f5d96a5c3386fcf76adc256473e
Yvon Quemener <yvon.quemener@csdps.qc.ca>
2020-01-13 10:11:49 -0500

    Màj version

--- src/version.php
+++ src/version.php
@@ -36,7 +36,7 @@ $plugin->requires = 2018050800;
 $plugin->component = 'theme_recit';
 
 // This is the named version.
-$plugin->release = '3.5.0';
+$plugin->release = 'R3-2020011300';
 
 // This is a stable release.
 $plugin->maturity = MATURITY_STABLE;
