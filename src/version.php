7e81eb8cc46731cc54c3a86285f0de3899737fb4
Yvon Quemener <yvon.quemener@csdps.qc.ca>
2020-02-13 16:29:40 -0500

    MàJ pour DEV2

--- src/version.php
+++ src/version.php
@@ -33,10 +33,10 @@ $plugin->requires = 2018050800;
 
 // This is the component name of the plugin - it always starts with 'theme_'
 // for themes and should be the same as the name of the folder.
-$plugin->component = 'theme_recit 20200214';
+$plugin->component = 'theme_recit';
 
 // This is the named version.
-$plugin->release = 'R1-2020020500';
+$plugin->release = 'R2-2020021400_';
 
 // This is a stable release.
 $plugin->maturity = MATURITY_STABLE;
