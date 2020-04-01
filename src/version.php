24aff3a709291fcd1ec091c18ac0fa4b332c66ae
Yvon Quemener <yvon.quemener@csdps.qc.ca>
2020-04-01 15:04:57 -0400

    Update version.php

--- src/version.php
+++ src/version.php
@@ -36,7 +36,7 @@ $plugin->requires = 2018050800;
 $plugin->component = 'theme_recit';
 
 // This is the named version.
-$plugin->release = 'R5-2020032702';
+$plugin->release = 'R5-2020033101';
 
 // This is a stable release.
 $plugin->maturity = MATURITY_STABLE;
