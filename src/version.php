1a1484866522eff16d38f0d3f763b118d26f8acb
Yvon Quemener <yvon.quemener@csdps.qc.ca>
2020-05-05 19:04:23 -0400

    Correctif etiquette V2

--- src/version.php
+++ src/version.php
@@ -36,7 +36,7 @@ $plugin->requires = 2018050800;
 $plugin->component = 'theme_recit';
 
 // This is the named version.
-$plugin->release = 'R7-2020050400';
+$plugin->release = 'R7-2020050500';
 
 // This is a stable release.
 $plugin->maturity = MATURITY_BETA;
