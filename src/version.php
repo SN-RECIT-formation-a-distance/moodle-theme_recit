35cdf8d6985a90ba3d1056c35aee99d10fe969e9
Yvon Quemener <yvon.quemener@csdps.qc.ca>
2020-07-20 10:56:48 -0400

    r9

--- src/version.php
+++ src/version.php
@@ -36,7 +36,7 @@ $plugin->requires = 2018050800;
 $plugin->component = 'theme_recit';
 
 // This is the named version.
-$plugin->release = 'R9-V1.9';
+$plugin->release = 'R9-V1.9.1';
 
 // This is a stable release.
 $plugin->maturity = MATURITY_BETA;
