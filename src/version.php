94100b30355da1a251b139001f6b2facf5580a42
Yvon Quemener <yvon.quemener@csdps.qc.ca>
2020-08-25 16:45:09 -0400

    r10

--- src/version.php
+++ src/version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 // This is the version of the plugin.
-$plugin->version = 2020062900;
+$plugin->version = 2020082500;
 
 // This is the version of Moodle this plugin requires.
 $plugin->requires = 2018050800;
@@ -36,7 +36,7 @@ $plugin->requires = 2018050800;
 $plugin->component = 'theme_recit';
 
 // This is the named version.
-$plugin->release = 'R9-V1.9.1';
+$plugin->release = 'R10-V1.9.2';
 
 // This is a stable release.
 $plugin->maturity = MATURITY_BETA;
