9ea83e8b0c41ccde5667adfd3a8d7df90dfaafdb
Gustavo Bazzo <gcbazzo@gmail.com>
2021-04-21 14:53:29 -0400

    Couleurs btn-primary math

--- src/version.php
+++ src/version.php
@@ -35,7 +35,7 @@ $plugin->requires = 2018050800;
 $plugin->component = 'theme_recit';
 
 // This is the named version.
-$plugin->release = 'R12-V1.12.7';
+$plugin->release = 'R12-V1.12.8';
 
 // This is a stable release.
 $plugin->maturity = MATURITY_BETA;
