c0d244eb1cc55b75d9b78df7a5972a854049367e
Gustavo Bazzo <gcbazzo@gmail.com>
2021-08-05 10:15:54 -0400

    Update version.php

--- src/version.php
+++ src/version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 // This is the version of the plugin.
-$plugin->version = 2021060701;
+$plugin->version = 2021072700;
 
 // This is the version of Moodle this plugin requires.
 $plugin->requires = 2018050800;
