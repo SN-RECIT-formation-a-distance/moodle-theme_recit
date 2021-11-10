c44aed79741009f5073e6528929eee42427b4bb0
Gustavo Bazzo <gcbazzo@gmail.com>
2021-11-10 15:12:02 -0500

    Update version.php

--- src/version.php
+++ src/version.php
@@ -35,7 +35,7 @@ $plugin->requires = 2018050800;
 $plugin->component = 'theme_recit';
 
 // This is the named version.
-$plugin->release = 'R14-V1.3';
+$plugin->release = 'R14-V1.4';
 
 // This is a stable release.
 $plugin->maturity = MATURITY_BETA;
